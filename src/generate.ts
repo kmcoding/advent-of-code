4fa656a18b71f75039198e7a1cf4e85d5c5045d4
Kevin Maucy <kmcoding@users.noreply.github.com>
2022-12-21 11:12:55 +0100

    Initial setup (incl. new gitattributes and better eslintrc and prettierrc)

--- src/generate.ts
+++ src/generate.ts
@@ -1,54 +1,55 @@
-import fs from 'fs';
-import path from 'path';
-import axios from 'axios';
+import fs from "fs";
+import path from "path";
+import axios from "axios";
 
-import { AOC_SESSION, YEAR } from './constants';
+import { AOC_SESSION, YEAR } from "./constants";
 
-let day;
+let day: string;
 
 // Grab day and task
 try {
-	day = process.argv[2].split('=')[1];
+  day = process.argv[2].split("=")[1];
 } catch (error) {
-	throw new Error('Missing param: "day"');
+  throw new Error('Missing param: "day"');
 }
 
 try {
-	(async () => {
-		if (!AOC_SESSION) {
-			return console.error('Session token from adventofcode.com missing.');
-		}
+  (async () => {
+    if (!AOC_SESSION) {
+      return console.error("Session token from adventofcode.com missing.");
+    }
 
-		if (!day || !YEAR) {
-			return console.error('Missing day or year');
-		}
+    if (!day || !YEAR) {
+      return console.error("Missing day or year");
+    }
 
-		// If day folder already exists, abort. Otherwise, create the folder.
-		const folderPath = path.resolve(__dirname, `${YEAR}/${day}`);
-		if (fs.existsSync(folderPath)) {
-			return console.warn(
-				'Folder already exists. To start over, remove the folder you are trying to generate.'
-			);
-		}
-		fs.mkdirSync(folderPath, { recursive: true });
+    // If day folder already exists, abort. Otherwise, create the folder.
+    const dayPadded = day.padStart(2, "0");
+    const folderPath = path.resolve(__dirname, `${YEAR}/${dayPadded}`);
+    if (fs.existsSync(folderPath)) {
+      return console.warn(
+        "Folder already exists. To start over, remove the folder you are trying to generate."
+      );
+    }
+    fs.mkdirSync(folderPath, { recursive: true });
 
-		// Fetch input data and write it to an input.txt file.
-		const inputData = await axios.get(
-			`https://adventofcode.com/${YEAR}/day/${day}/input`,
-			{ headers: { cookie: `session=${AOC_SESSION}` } }
-		);
-		fs.writeFileSync(path.resolve(folderPath, 'input.txt'), inputData.data);
+    // Fetch input data and write it to an input.txt file.
+    const inputData = await axios.get(
+      `https://adventofcode.com/${YEAR}/day/${day}/input`,
+      { headers: { cookie: `session=${AOC_SESSION}` } }
+    );
+    fs.writeFileSync(path.resolve(folderPath, "input.txt"), inputData.data);
 
-		// Copy template file.
-		fs.copyFileSync(
-			path.resolve(__dirname, 'template.ts'),
-			path.resolve(folderPath, '1.ts')
-		);
+    // Copy template file.
+    fs.copyFileSync(
+      path.resolve(__dirname, "template.ts"),
+      path.resolve(folderPath, "1.ts")
+    );
 
-		console.log(`Generated folder for year ${YEAR}, day ${day}. Good luck!`);
-	})();
+    console.log(`Generated folder for year ${YEAR}, day ${day}. Good luck!`);
+  })();
 } catch (error) {
-	throw new Error('Failed to generate template folder.');
+  throw new Error("Failed to generate template folder.");
 }
 
 export {};
