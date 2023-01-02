4e1effcf97637476c2a600637effa199f21ac3a8
Kevin Maucy <kmcoding@users.noreply.github.com>
2023-01-02 17:14:01 +0100

    Solution 2022 day 6 part 1

--- src/2022/06/index.ts
+++ src/2022/06/index.ts
@@ -4,10 +4,39 @@ async function readInput(filename: string) {
   return await fs.readFile(filename, "utf8");
 }
 
+function findMarker(dataStream: string, bufferSize: number): number {
+  let marker = bufferSize;
+
+  for (let index = 0; index < dataStream.length - bufferSize; index++) {
+    const buffer = dataStream.slice(index, index + bufferSize);
+    const duplicates = buffer
+      .split("")
+      .sort()
+      .join("")
+      .match(/(.)\1+/g);
+    if (duplicates === null) {
+      marker += index;
+      break;
+    }
+  }
+
+  return marker;
+}
+
+async function solveSample() {
+  const data = await readInput("sample.txt");
+
+  data.split("\n").forEach((line) => {
+    const resultMarker = findMarker(line, 4);
+    console.log(resultMarker);
+  });
+}
+
 async function solve1() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const data = await readInput("input.txt");
-  // do magic here...
+  const resultMarker = findMarker(data, 4);
+  console.log(resultMarker);
 }
 
 async function solve2() {
@@ -16,5 +45,6 @@ async function solve2() {
   // do magic here...
 }
 
+// solveSample();
 solve1();
-solve2();
+// solve2();
